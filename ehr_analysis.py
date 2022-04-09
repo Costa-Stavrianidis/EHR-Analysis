6a199b5716b28c1170f7483dcb89106e53e3367f
Costa-Stavrianidis <87909649+Costa-Stavrianidis@users.noreply.github.com>
2022-04-09 12:49:40 -0400

    Updated analysis to use dictionaries

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -1,9 +1,10 @@
-"""Computational Complexity/Data Structures."""
+"""EHR Analysis."""
 
 from datetime import datetime
 
 
 class Patient:
+
     """Patient class with instance attributes."""
 
     def __init__(self, ID: [str], gender: [str], DOB: [str], race: [str]):
@@ -33,49 +34,59 @@ class Lab:
         self.admission = admission
 
 
-def parse_data(filename: str, data_type: str) -> list[Patient] | list[Lab]:
-    """Read and parse the patient or lab data file."""
+def parse_patient_data(filename: str) -> dict[str:Patient]:
+    """Read and parse the patient data file."""
     # Header row must have variables named exactly as defined below
-    if data_type == "Patient":  # O(1)
-        with open(filename, "r", encoding="utf-8-sig") as patient_file:  # O(1)
-            patient_lines = patient_file.readlines()  # O(N)
-            patient_file_list = []  # O(1)
-            for row in patient_lines:  # O(N)
-                row = row.split("\t")  # O(1)
-                patient_file_list.append(row)  # O(1)
-            patient_ID_col_index = patient_file_list[0].index("PatientID")  # O(N)
-            patient_gender_col_index = patient_file_list[0].index(
-                "PatientGender"
-            )  # O(N)
-            patient_DOB_col_index = patient_file_list[0].index(
-                "PatientDateOfBirth"
-            )  # O(N)
-            patient_race_col_index = patient_file_list[0].index("PatientRace")  # O(N)
-            patient_list = []  # O(1)
-            for patients in patient_file_list[1:]:  # O(N)
-                patient_list.append(
-                    Patient(
-                        patients[patient_ID_col_index],
-                        patients[patient_gender_col_index],
-                        patients[patient_DOB_col_index],
-                        patients[patient_race_col_index],
+    with open(filename, "r", encoding="utf-8-sig") as patient_file:  # O(1)
+        patient_lines = patient_file.readlines()  # O(N)
+        patient_file_list = []  # O(1)
+        for row in patient_lines:  # O(N)
+            row = row.split("\t")  # O(1)
+            patient_file_list.append(row)  # O(1)
+        patient_ID_col_index = patient_file_list[0].index("PatientID")  # O(N)
+        patient_gender_col_index = patient_file_list[0].index("PatientGender")  # O(N)
+        patient_DOB_col_index = patient_file_list[0].index("PatientDateOfBirth")  # O(N)
+        patient_race_col_index = patient_file_list[0].index("PatientRace")  # O(N)
+        patient_dict = dict()  # O(1)
+        for patients in patient_file_list[1:]:  # O(N)
+            patient_dict[patients[patient_ID_col_index]] = Patient(
+                patients[patient_ID_col_index],
+                patients[patient_gender_col_index],
+                patients[patient_DOB_col_index],
+                patients[patient_race_col_index],
+            )  # O(4)
+    return patient_dict  # O(1)
+
+
+# 1 + N + 1 + N * (2) + N + N + N + N + 1 + N * (4) + 1 -> 11(N) + 4 -> O(N)
+
+
+def parse_lab_data(filename: str) -> dict[str : list[Lab]]:
+    """Read and parse the lab data file."""
+    # Header row must have variables named exactly as defined below
+    with open(filename, "r", encoding="utf-8-sig") as lab_file:  # O(1)
+        lab_lines = lab_file.readlines()  # O(M)
+        lab_file_list = []  # O(1)
+        for row in lab_lines:  # O(M)
+            row = row.split("\t")  # O(1)
+            lab_file_list.append(row)  # O(1)
+        lab_ID_col_index = lab_file_list[0].index("PatientID")  # O(M)
+        lab_test_col_index = lab_file_list[0].index("LabName")  # O(M)
+        lab_value_col_index = lab_file_list[0].index("LabValue")  # O(M)
+        lab_admission_col_index = lab_file_list[0].index("LabDateTime\n")  # O(M)
+        lab_dict = dict()  # O(1)
+        for labs in lab_file_list[1:]:  # O(M)
+            if labs[lab_ID_col_index] not in lab_dict:  # O(1)
+                lab_dict[labs[lab_ID_col_index]] = [
+                    Lab(
+                        labs[lab_ID_col_index],
+                        labs[lab_test_col_index],
+                        labs[lab_value_col_index],
+                        labs[lab_admission_col_index],
                     )
-                )  # O(4)
-        return patient_list  # O(1)
-    elif data_type == "Lab":  # O(1)
-        with open(filename, "r", encoding="utf-8-sig") as lab_file:  # O(1)
-            lab_lines = lab_file.readlines()  # O(M)
-            lab_file_list = []  # O(1)
-            for row in lab_lines:  # O(M)
-                row = row.split("\t")  # O(1)
-                lab_file_list.append(row)  # O(1)
-            lab_ID_col_index = lab_file_list[0].index("PatientID")  # O(M)
-            lab_test_col_index = lab_file_list[0].index("LabName")  # O(M)
-            lab_value_col_index = lab_file_list[0].index("LabValue")  # O(M)
-            lab_admission_col_index = lab_file_list[0].index("LabDateTime\n")  # O(M)
-            lab_list = []  # O(1)
-            for labs in lab_file_list[1:]:  # O(M)
-                lab_list.append(
+                ]  # O(4)
+            else:  # O(1)
+                lab_dict[labs[lab_ID_col_index]].append(
                     Lab(
                         labs[lab_ID_col_index],
                         labs[lab_test_col_index],
@@ -83,34 +94,29 @@ def parse_data(filename: str, data_type: str) -> list[Patient] | list[Lab]:
                         labs[lab_admission_col_index],
                     )
                 )  # O(4)
-        return lab_list  # O(1)
+    return lab_dict  # O(1)
 
 
-# 1 + 1 + N + 1 + N * (1 + 1) + N + N + N + N + 1 + N * (4) + 1 +
-# 1 + 1 + M + 1 + M * (1 + 1) + M + M + M + M + 1 + M * (4) + 1 -> 11(N) + 11(M) -> O(N) + O(M)
+# 1 + M + 1 + M * (2) + M + M + M + M + 1 + M * (8) + 1 -> 15(M) + 4 -> O(M)
 
-# This function determines whether the data is patient or lab data, and then
-# it opens the file and create a list, with every element of the list
-# being a row in the data file. It then uses the header row of the data file
-# to find which index of each row contains the variable named in the header row.
-# After finding the indexes for the variables, the function iterates through the
-# list of rows, taking the elements of the rows and defining the custom class
-# (Patient or Lab) attributes as the elements of the rows that match the
-# attributes. This will create a Patient or Lab object for each row in the dataset.
-# The function then returns a list of Patient objects or a list of Lab objects. The
-# overall complexity is O(N) + O(M). Note that to add more columns of the data as
-# attributes, simply add the attributes to the Patient or Lab class, define the index
-# of the column in the function, and then add the attribute to the Patient or Lab
-# class in the function using that index.
+# These functions open the file and create a dictionary, with every key
+# being a patient ID. The values of the dictionary in the patient parsing function
+# are the Patient objects for that particular patient ID. The values in the lab parsing
+# function are a list of all the Lab objects for that particular patient ID.
+# The functions use the header row of the data file to find which index of each row
+# contains the variable named in the header row. The overall complexity is O(N) or O(M).
+# Note that to add more columns of the data as attributes, simply add the attributes to
+# the Patient or Lab class, define the index of the column in the function, and then add
+# the attribute to the Patient or Lab class in the function using that index.
 
 
-def num_older_than(age: float, patient_data: list[Patient]) -> int:
+def num_older_than(age: float, patient_data: dict[str:Patient]) -> int:
     """Take data and return the number of patients older
     than a given age (in years).
 
     """
     older = 0  # O(1)
-    for patients in patient_data:  # O(N)
+    for patients in patient_data.values():  # O(N)
         if patients.age > age:  # O(1)
             older += 1  # O(1)
     return older  # O(1)
@@ -118,15 +124,15 @@ def num_older_than(age: float, patient_data: list[Patient]) -> int:
 
 # 1 + N * (1 * 1) + 1 -> N + 3 -> O(N)
 
-# This function takes the patient data in the form of a list of
-# Patient objects. It will then iterate through this list and find
-# how many patients have an age property (their age as of today in
-# years) that is above the given input age in the function. The
-# overall complexity is O(N), so linear time.
+# This function takes the patient data in the form of a dictionary, with
+# the dictionary keys being patient ID's, and the values being Patient objects.
+# It will then iterate through this list and find how many patients have an
+# age property (their age as of today in years) that is above the given input
+# age in the function. The overall complexity is O(N), so linear time.
 
 
 def sick_patients(
-    lab: str, gt_lt: str, lab_value: float, lab_data: list[Lab]
+    lab: str, gt_lt: str, lab_value: float, lab_data: dict[str : list[Lab]]
 ) -> list[str]:
     """Take data and return a unique list of patients who have a given test
     with value above (">") or below ("<") a given level.
@@ -134,9 +140,10 @@ def sick_patients(
     """
     patients = []  # O(1)
     patients_sick = set()  # O(1)
-    for labs in lab_data:  # O(N)
-        if labs.test == lab:  # O(1)
-            patients.append(labs)  # O(1)
+    for labs in lab_data.values():  # O(N)
+        for patient_labs in labs:  # O(M)
+            if patient_labs.test == lab:  # O(1)
+                patients.append(patient_labs)  # O(1)
     for rows in patients:  # O(N)
         rows.value = float(rows.value)  # O(1)
         if gt_lt == ">":  # O(1)
@@ -149,31 +156,26 @@ def sick_patients(
     return patients_sick  # O(1)
 
 
-# 1 + 1 + N * (1 * 1) + N * (1 + 1 * 1 * 1 + 1 * 1 * 1) + 1 + 1 -> 5N + 4 -> O(N)
+# 1 + 1 + N * (M) + N * (1 + 1 * 1 * 1 + 1 * 1 * 1) + 1 + 1 -> O(NM)
 
-# This function takes data in the form of a list of Lab objects. It
-# will iterate through the list of of Lab objects and create a unique
+# This function takes data in the form of a dictionary of patient ID's and
+# Lab objects. It will iterate through the list of of Lab objects and create a unique
 # list of strings that are patient ID's of patients that have a certain
-# lab test value above or below a given level. The overall complexity is O(N),
-# which is linear time.
+# lab test value above or below a given level. The overall complexity is O(NM).
 
 
 def admission_age(
-    patient: str, patient_data: list[Patient], lab_data: list[Lab]
+    patient: str, patient_data: dict[str:Patient], lab_data: dict[str : list[Lab]]
 ) -> int:
-    """Take lab and patient data and return the age at first admission
+    """Take patient and lab data and return the age at first admission
     of any given patient.
 
     """
-    for patients in patient_data:
-        if patients.ID == patient:
-            patient_DOB = datetime.strptime(patients.DOB, "%Y-%m-%d %H:%M:%S.%f")
+    patient_DOB = datetime.strptime(patient_data[patient].DOB, "%Y-%m-%d %H:%M:%S.%f")
     admissions = []
-    for labs in lab_data:
-        if labs.ID == patient:
-            admissions.append(
-                datetime.strptime(labs.admission[:23], "%Y-%m-%d %H:%M:%S.%f")
-                - patient_DOB
-            )
+    for labs in lab_data[patient]:
+        admissions.append(
+            datetime.strptime(labs.admission[:23], "%Y-%m-%d %H:%M:%S.%f") - patient_DOB
+        )
     age = int(min(admissions).days / 365.2425)
     return age
--- ehr_testing.py
+++ ehr_testing.py
@@ -3,23 +3,38 @@
 import pytest
 
 from ehr_analysis import (
-    parse_data,
+    parse_patient_data,
+    parse_lab_data,
     num_older_than,
     sick_patients,
     admission_age,
 )
 
 
-def test_parse_data():
-    """Test parsing patient and lab data."""
+def test_parse_patient_data():
+    """Test parsing patient data."""
     filename1 = "patient_test_data.txt"
+    assert (
+        parse_patient_data(filename1)["DB92CDC6-FA9B-4492-BC2C-0C588AD78956"].ID
+        == "DB92CDC6-FA9B-4492-BC2C-0C588AD78956"
+    )
+    assert (
+        parse_patient_data(filename1)["56A35E74-90BE-44A0-B7BA-7743BB152133"].race
+        == "White"
+    )
+
+
+def test_parse_lab_data():
+    """Test parsing lab data."""
     filename2 = "lab_test_data.txt"
     assert (
-        parse_data(filename1, "Patient")[0].ID == "DB92CDC6-FA9B-4492-BC2C-0C588AD78956"
+        parse_lab_data(filename2)["DB92CDC6-FA9B-4492-BC2C-0C588AD78956"][1].test
+        == "CBC: MONOCYTES"
     )
-    assert parse_data(filename1, "Patient")[2].race == "White"
-    assert parse_data(filename2, "Lab")[0].ID == "DB92CDC6-FA9B-4492-BC2C-0C588AD78956"
-    assert parse_data(filename2, "Lab")[1].value == "0.3"
+    assert (
+        parse_lab_data(filename2)["56A35E74-90BE-44A0-B7BA-7743BB152133"][0].value
+        == "17"
+    )
 
 
 def test_num_older_than():
@@ -28,7 +43,7 @@ def test_num_older_than():
     age2 = 150
     age3 = 52
     age4 = 60
-    patient_data = parse_data("patient_test_data.txt", "Patient")
+    patient_data = parse_patient_data("patient_test_data.txt")
     assert num_older_than(age1, patient_data) == 3
     assert num_older_than(age2, patient_data) == 0
     assert num_older_than(age3, patient_data) == 2
@@ -43,7 +58,7 @@ def test_sick_patients():
     gt_lt2 = "<"
     value1 = 0.2
     value2 = 18
-    lab_data = parse_data("lab_test_data.txt", "Lab")
+    lab_data = parse_lab_data("lab_test_data.txt")
     assert "79A7BA2A-D35A-4CB8-A835-6BAA13B0058C" in sick_patients(
         lab1, gt_lt1, value1, lab_data
     )
@@ -56,7 +71,7 @@ def test_admission_age():
     """Test returning age at first admission of given patient."""
     patient1 = "DB92CDC6-FA9B-4492-BC2C-0C588AD78956"
     patient2 = "79A7BA2A-D35A-4CB8-A835-6BAA13B0058C"
-    patient_data = parse_data("patient_test_data.txt", "Patient")
-    lab_data = parse_data("lab_test_data.txt", "Lab")
+    patient_data = parse_patient_data("patient_test_data.txt")
+    lab_data = parse_lab_data("lab_test_data.txt")
     assert admission_age(patient1, patient_data, lab_data) == 15
     assert admission_age(patient2, patient_data, lab_data) == 27
